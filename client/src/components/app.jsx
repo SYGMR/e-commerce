91503f62792c3b5d97593498b46bf8ec39fa2e82
yamuniyamarezu <yamuniyamarezu@planet.earth>
2019-12-22 09:37:27 +0100

    Took loginForm from home page and put it into its own component now accessible through /login

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -7,6 +7,7 @@ import LandingPage from './landing-page';
 import AdminPage from './admin-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
+import LoginPage from './login-page';
 import {refreshAuthToken} from '../actions/auth';
 
 export class App extends React.Component {
@@ -46,6 +47,7 @@ export class App extends React.Component {
 								<Route exact path="/" component={LandingPage} />
 								<Route exact path="/dashboard" component={Dashboard} />
 								<Route exact path="/register" component={RegistrationPage} />
+								<Route exact path="/login" component={LoginPage} />
 								<Route exact path="/admin" component={AdminPage} />
 						</div>
 				);
