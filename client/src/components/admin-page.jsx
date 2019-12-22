f2e3a8fec56931e893b0c977e504f4e875d978e5
Carlos sebastian Brito conti <carlos-sebastian.brito-conti@epitech.eu>
2019-12-22 18:49:16 +0100

    icons admin-page

--- client/src/components/admin-page.jsx
+++ client/src/components/admin-page.jsx
@@ -1,19 +1,110 @@
 import React from 'react';
-import { HydraAdmin, ResourceGuesser } from '@api-platform/admin';
+
+import {
+	ShoppingCart, 
+	AddShoppingCart, 
+	Class, 
+	Person, 
+	VerifiedUser, 
+	Apps, 
+	RecordVoiceOver, 
+	FormatListBulleted, 
+	Store, 
+	GridOn, 
+	Star 
+} from '@material-ui/icons';
+
+import UserIcon from '@material-ui/icons/Group';
+
+import { 
+	HydraAdmin, 
+	ResourceGuesser,
+	ListGuesser,
+	CreateGuesser,
+	FieldGuesser,
+	EditGuesser,
+	InputGuesser
+ } from '@api-platform/admin';
+
+ 
+// ---------------- Categories ----------------//
+const CategorieCreate = props => (
+	<CreateGuesser {...props}>
+	  <InputGuesser source="name" />
+	</CreateGuesser>
+);
+
+const CategoriesEdit = props => (
+	<EditGuesser  {...props}>
+	  <InputGuesser  source="name" />
+  </EditGuesser >
+);
+
+// ---------------- Customers ----------------//
+
+
+
+// ---------------- User ----------------//
+const UsersEdit = props => (
+	<EditGuesser  {...props}>
+	  <InputGuesser  source="username" />
+	  <InputGuesser  source="email" />
+	  <InputGuesser  source="password" value={""} />
+	  <InputGuesser  source="roles" />
+  </EditGuesser >
+);
+
+const UseerCreate = props => (
+	<CreateGuesser {...props}>
+	  <InputGuesser  source="username" />
+	  <InputGuesser  source="email" />
+	  <InputGuesser  source="password" />
+	  <InputGuesser  source="roles" />
+	</CreateGuesser>
+);
+
 
 export default () => (
 	<HydraAdmin entrypoint={process.env.REACT_APP_API_BASE_URL}>
-		<ResourceGuesser name="carts" />
-		<ResourceGuesser name="cart_items" />
-		<ResourceGuesser name="categories" />
-		<ResourceGuesser name="customers" />
-		<ResourceGuesser name="merchants" />
-		<ResourceGuesser name="products" />
-		<ResourceGuesser name="sellers" />
-		<ResourceGuesser name="seller_items" />
-		<ResourceGuesser name="shops" />
-		<ResourceGuesser name="shop_items" />
-		<ResourceGuesser name="statuses" />
-		<ResourceGuesser name="users" />
+		<ResourceGuesser name="carts" 
+			icon={ShoppingCart}
+		/>
+		<ResourceGuesser name="cart_items" 
+			icon={AddShoppingCart}
+		/>
+		<ResourceGuesser name="categories" 
+			create={CategorieCreate}
+			edit={CategoriesEdit}
+			icon={Class}
+		/>
+		<ResourceGuesser name="customers" 
+			icon={Person}
+		/>
+		<ResourceGuesser name="merchants" 
+			icon={VerifiedUser}
+		/>
+		<ResourceGuesser name="products" 
+			icon={Apps}
+		/>
+		<ResourceGuesser name="sellers" 
+			icon={RecordVoiceOver}
+		/>
+		<ResourceGuesser name="seller_items" 
+			icon={FormatListBulleted}
+		/>
+		<ResourceGuesser name="shops" 
+			icon={Store}
+		/>
+		<ResourceGuesser name="shop_items" 
+			icon={GridOn}Star
+		/>
+		<ResourceGuesser name="statuses" 
+			icon={Star}
+		/>
+		<ResourceGuesser name="users" 
+			edit={UsersEdit}
+			create={UseerCreate}
+			icon={UserIcon}
+		/>
 	</HydraAdmin>
 );
