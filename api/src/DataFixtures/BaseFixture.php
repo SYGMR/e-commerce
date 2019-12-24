a140e423e2c715eb8625f91b759d3f64dbdd8288
yamuniyamarezu <yamuniyamarezu@planet.earth>
2019-12-24 12:39:11 +0100

    Added product fixtures

--- api/src/DataFixtures/BaseFixture.php
+++ api/src/DataFixtures/BaseFixture.php
@@ -13,6 +13,8 @@ abstract class BaseFixture extends Fixture
 	/** @var Generator */
 	protected $faker;
 
+	private $referencesIndex = [];
+
 	abstract protected function loadData(ObjectManager $manager);
 
 	public function load(ObjectManager $manager)
@@ -32,5 +34,21 @@ abstract class BaseFixture extends Fixture
 			// store for usage later as App\Entity\ClassName_#COUNT#
 			$this->addReference($className . '_' . $i, $entity);
 		}
+	}
+
+	protected function getRandomReference(string $className) {
+		if (!isset($this->referencesIndex[$className])) {
+				$this->referencesIndex[$className] = [];
+				foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+						if (strpos($key, $className.'_') === 0) {
+								$this->referencesIndex[$className][] = $key;
+						}
+				}
+		}
+		if (empty($this->referencesIndex[$className])) {
+				throw new \Exception(sprintf('Cannot find any references for class "%s"', $className));
+		}
+		$randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$className]);
+		return $this->getReference($randomReferenceKey);
 	}
 }
---
+++ api/src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ProductFixtures extends BaseFixture
+{
+		public function loadData(ObjectManager $manager)
+		{
+				$this->createMany(Product::class, 150, function(Product $category, $count) {
+						$category->setName($this->faker->productName)
+							->setDescription($this->faker->text)
+							->setPrice($this->faker->numberBetween(1, 9000))
+							->setCategory($this->getRandomReference(Category::class));
+							/*->setImages([])*/
+				});
+
+				$manager->flush();
+		}
+}
